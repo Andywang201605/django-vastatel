164d69909e4409e2f93bc1040e38f074ad430f84
Andywang201605 <ztwang201605@gmail.com>
2022-01-27 21:12:51 +1100

    update normal match, add TNSQuery

--- atel/crawler/coordmatch.py
+++ atel/crawler/coordmatch.py
@@ -1,10 +1,13 @@
 ### this is a separate script mainly focus on matching coordinates in html/txt
 import logging
 import re
-import logging
+from bs4 import BeautifulSoup
+import requests
 
 from astropy.coordinates import SkyCoord
 import astropy.units as u
+
+import time
 
 
 class CoordMatch:
@@ -27,11 +30,11 @@ class CoordMatch:
         ### format for R.A.
         #           #   RA or R.A.  #          #         (J2000.0)       #          #      date       #
         self.startRA = r'''(((R\.?\s*A\.?\s*|Coord\s*)(\(?\s*J2000(\.0)?\s*\)?\s*)?(=|:|\s)|(\d{4}-\d{2}-\d{2}))\s*)?'''
-        self.endRA = r'''\s*(deg)?(d)?(and)?(,)?(;)?(o)?\s*'''
+        self.endRA = r'''\s*(deg)?(d)?(and)?(,)?(;)?(o)?\s+'''
         # end with deg, d and and
         ### format for Decl.
         self.startDec = r'''\s*(Decl?\.?\s*(=|:)?)?\s*'''
-        self.endDec = r'''\s*(deg)?(d)?(o)?'''
+        self.endDec = r'''(\s*deg)?(\s*d)?(\s*o)?'''
 
     def matchSex(self):
         '''
@@ -58,7 +61,7 @@ class CoordMatch:
                             )
                     )"""
 
-        reSex = r'''(?P<coordSex> %s %s %s %s %s %s)''' % (self.startRA, raSex, self.endRA, self.startDec, decSex, self.endDec)
+        reSex = r'''(?:(?<=^)|(?<=\s)|(?<=\>))(?P<coordSex> %s %s %s %s %s %s)''' % (self.startRA, raSex, self.endRA, self.startDec, decSex, self.endDec)
         reSexEx = re.compile(reSex, re.S | re.I | re.X)
 
         ### convert match group to coordinate ###
@@ -85,9 +88,9 @@ class CoordMatch:
             each element is the raw text for a given coordinate in coordStrings
         '''
         # TODO: remove Sex match first and then perform Deg match
-        raDeg = r'''(?P<raDeg>(([0-3]\d{2})|(\d{2})|\d)(\.\d{1,})?)'''
-        decDeg = r'''(?P<decDeg>[\+\-\–]?([0-8]?\d(\.\d{1,})?))'''
-        reDeg = r'''(?P<coordDeg> %s %s %s %s %s %s)''' % (self.startRA, raDeg, self.endRA, self.startDec, decDeg, self.endDec)
+        raDeg = r'''(?P<raDeg>(([0-3]\d{2})|(\d{2})|\d)(\.\d{3,}))'''
+        decDeg = r'''(?P<decDeg>[\+\-\–]?([0-8]?\d(\.\d{3,})))''' # given the precision of the instruments, we assert that all decimal coordinates are with decimal places
+        reDeg = r'''(?:(?<=^)|(?<=\s)|(?<=\>))(?P<coordDeg> %s %s %s %s %s %s)''' % (self.startRA, raDeg, self.endRA, self.startDec, decDeg, self.endDec)
         reDegEx = re.compile(reDeg, re.S | re.I | re.X)
 
         ### convert match group to coordinate ###
@@ -99,3 +102,61 @@ class CoordMatch:
             coordStrings.append(coordStr); coordRaws.append(matchDict['coordDeg'])
         return coordStrings, coordRaws
 
+
+class TNSQuery:
+    '''
+    Literally we need to use TNS-API to do so. The `TNSQuery` class here uses requests module to achieve that (NOT RECOMMENDED) 
+    '''
+    def __init__(self, tnsname, module='main'):
+        self.logger = logging.getLogger(f'{module}.tnsquery')
+        self.tnsname = tnsname
+        pass
+        
+    def _getresponse_(self):
+        ### use requests to get response
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+        tnsurl = f'https://www.wis-tns.org/object/{self.tnsname}'
+        r = requests.get(tnsurl, headers=headers)
+        self.logger.info(f'fetching data from TNS server... status code {r.status_code}')
+        if r.status_code == 404: raise ValueError("The tns name is invalid or TNS server is not accessable for now...")
+        assert r.status_code == 200, "cannot request data from tns for object {} - status_code {}".format(self.tnsname, r.status_code)
+        ### convert response to BeautifulSoup Object
+        self.soup = BeautifulSoup(r.text, 'html.parser')
+
+    def _findcoord_(self):
+        '''find coordinate from html'''
+        _field = self.soup.findAll('div', attrs={'class':'field field-radec'})
+        assert len(_field) > 0, f"no radec field found... Perhaps you get a wrong object name ({self.tnsname})?"
+        return _field[0].findAll('div', attrs={'class':'value'})[0].text
+
+    def checkcoord(self, sleeptime=60, max_attempt=3):
+        '''
+        check coordinate for a given TNS object
+
+        Params:
+        ----------
+        sleeptime: int or float
+            seconds for the system to get another attempt if failed
+        max_attempt: int
+            the maximum attempt for a single try
+        
+        Returns:
+        ----------
+        coord: string
+            coordinate of the object - in the format of HH:MM:SS.SSS DD:MM:SS.SS
+        '''
+        ### try get the html page
+        tnsrespond = False; attempt = 0
+        while not tnsrespond:
+            try:
+                self._getresponse_()
+                tnsrespond = True   # get response successfully
+            except ValueError:
+                self.logger.critical(f'The tns name {self.tnsname} is invalid or TNS server is not accessable for now...')
+            except AssertionError:
+                if attempt > max_attempt: raise ValueError(f'Reached maximum ({max_attempt}) attempts, Abort!')
+                time.sleep(sleeptime) # sleep and attempt again
+                attempt = attempt + 1
+
+        ### parse coordinate
+        return self._findcoord_()
