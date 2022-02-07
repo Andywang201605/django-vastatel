ae575b761e9fcac3d0366a9483898df5aa3b706a
Andywang201605 <ztwang201605@gmail.com>
2022-02-07 18:55:36 +1100

    add download features

--- AstroCrawler/crawlerUtils.py
+++ AstroCrawler/crawlerUtils.py
@@ -46,3 +46,38 @@ def makedirs(path):
     '''
     if not os.path.exists(path):
         os.makedirs(path)
+
+### response downloading related ###
+import requests
+import time
+
+def _requestURL_(url):
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+    response = requests.get(url, headers=headers)
+    assert response.status_code == 200, f'Unsuccessful attempt for request "{url}"'
+    return response
+
+def _responseText_(response, filepath):
+    '''save response.text to a file'''
+    with open(filepath, 'w', encoding='utf-8') as fp:
+        fp.write(response.text)
+
+def downloadURL(url, filepath, recursive=True, maxAttempt=5, sleepTime=10):
+    '''
+    download response.text from a given url
+    '''
+    if recursive: r = _recursiveRequest_(url, maxAttempt=maxAttempt, sleepTime=sleepTime)
+    else: r = _requestURL_(url)
+    _responseText_(r, filepath)
+
+def _recursiveRequest_(url, maxAttempt=5, sleepTime=10):
+    '''
+    recursively request a given url, with maximum attempt given in `maxAttempt`
+    '''
+    successRequest = False; attemptCount = 0
+    while not successRequest:
+        try: r = _requestURL_(url); return r
+        except:
+            attemptCount += 1
+            if attemptCount > maxAttempt: raise ValueError('Request failed...')
+            time.sleep(sleepTime)
